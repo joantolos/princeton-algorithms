1ac41f0a938ffa1063688297883db77b6429c663
joantolos <joan.tolos@gmail.com>
2022-07-12 09:01:29 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+princeton-algorithms
+
+Assignments and code solutions for the course Princeton Algorithms Part 1. You can find it on Coursera:
+
+https://es.coursera.org/learn/algorithms-part1
---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        StdOut.println("Hello, World");
+    }
+
+}
